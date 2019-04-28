8af21ee47890e92aa6e19d36292616bb5f3a963c
Yossi <yossi.kayhko@gmail.com>
2019-04-28 11:23:03 +0300

    first commit

---
+++ chatsapp/talkbot/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+
+from django.urls import include, path
+
+
+
+
+from .import views
+
+
+app_name= 'talkbot'
+urlpatterns = [
+    path('', views.homepage, name="homepage"),
+]
